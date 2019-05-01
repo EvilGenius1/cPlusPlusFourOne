4c10adc884a2e16718ce330548cc5160765e9adc
danykivanov <danykivanov@gmail.com>
2019-05-01 11:19:18 +0300

    Add project files.

---
+++ CplusFourOne/CplusFourOne.cpp
@@ -0,0 +1,20 @@
+﻿// CplusFourOne.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	for (int i = 0; i < s.length(); i++) {
+		if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')) {
+			s.erase(i, 1); i--;
+		}
+	}
+	cout << s;
+	
+	return 0;
+}
